357a027193dcb235624ffbc90aa9def88360ab18
Administrador <Administrador@Formacion-TOSH3.ipartekaula.com>
2017-09-07 09:40:46 +0200

    Introducido try-catch

--- src/CalcularIva.java
+++ src/CalcularIva.java
@@ -9,20 +9,32 @@ public class CalcularIva {
 		System.out.println("Calculadora del IVA ");
 		System.out.println("---------------------");
 		char letra = 'k';
+		float importe = 0;
+		boolean importeCorrecto = true;
 		
 		do {
 			System.out.println("Por favor dame el importe en euros");
+			//comprobar que sea un numero correcto
+			do {
+				try {
+					src = new Scanner(System.in);
+					importe = src.nextFloat();
+					importeCorrecto = false;
+				} catch (Exception e) {
+					System.out.println("valor incorrecto, introduce un valor correcto,por ejemplo: 123.45");
+				}
+				
+			} while(importeCorrecto);
 			
-			float importe = src.nextFloat();
-			
+			//calculo del iva
 			System.out.println("El IVA repercutido es : " + importe * 0.21);
+			
+			//si queremos recpetir la operacion
 			while(letra != 's' && letra != 'n') {
 				System.out.println("Quieres volver a calcular otro importe? Pulsa 's' para Seguir, 'n' para Parar");
 				String opcion = src.next();
 				letra = opcion.charAt(0);
-			}
-			
-			
+			}		
 		} while ('s' == letra);
 		
 		System.out.println("---------------------------");
