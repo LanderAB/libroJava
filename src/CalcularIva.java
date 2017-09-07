a478a0b75388371cf7d7f93685d4e90a79f301a7
Administrador <Administrador@Formacion-TOSH3.ipartekaula.com>
2017-09-07 09:08:51 +0200

    Cambios en el Scanner, variable "src" cerrada

--- src/CalcularIva.java
+++ src/CalcularIva.java
@@ -4,13 +4,14 @@ public class CalcularIva {
 
 	public static void main(String[] args) {
 		
+		Scanner src = new Scanner(System.in);
+		
 		System.out.println("Calculadora del IVA ");
 		System.out.println("---------------------");
 		char letra = 'k';
+		
 		do {
 			System.out.println("Por favor dame el importe en euros");
-			
-			Scanner src = new Scanner(System.in);
 			
 			float importe = src.nextFloat();
 			
@@ -26,6 +27,8 @@ public class CalcularIva {
 		
 		System.out.println("---------------------------");
 		System.out.println("Adios hasta la proxima");
+		
+		src.close();
 	}
 
 }
