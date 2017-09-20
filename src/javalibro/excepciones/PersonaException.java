ecf524fbf59cfceea4461dab284d6b293eb3bd46
Administrador <Administrador@Formacion-TOSH3.ipartekaula.com>
2017-09-20 13:57:12 +0200

    Excepcion personalizada

---
+++ src/javalibro/excepciones/PersonaException.java
@@ -0,0 +1,18 @@
+package javalibro.excepciones;
+
+public class PersonaException extends Exception{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public final static String MSG_EDAD_MENOR = "Debe ser mayor que 18";
+	public final static String MSG_EMAIL_ERROR = "Email no es correcto";
+	public final static String MSG_DNI_ERROR = "DNI no es correcto";
+	
+	public PersonaException(String messageException) {
+		super(messageException);
+	}
+	
+	
+
+}
